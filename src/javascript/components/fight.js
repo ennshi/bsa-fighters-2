b8a4b8cc0cd09fc78beb8f4c48b7090f0098b090
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-18 12:03:02 +0100

    Add createFighterStats

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,6 +3,9 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+    let winner = null;
+    const firstFighterStats = createFighterStats(firstFighter);
+    const secondFighterStats = createFighterStats(secondFighter);
   });
 }
 
@@ -16,4 +19,14 @@ export function getHitPower(fighter) {
 
 export function getBlockPower(fighter) {
   // return block power
+}
+
+function createFighterStats({_id, health, defense, attack}) {
+  return {
+    _id,
+    health,
+    defense,
+    attack,
+    canDoSuperAttack: true
+  }
 }
