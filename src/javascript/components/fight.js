62c1332a3f48ec479174926a30dfad5bfeb54db4
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-18 12:13:05 +0100

    Implement damage calculations

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -11,14 +11,27 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getDamage(attacker, defender) {
   // return damage
+  const damage = getHitPower(attacker) - getBlockPower(defender);
+  if(damage < 0) {
+    return 0;
+  }
+  return damage;
 }
 
 export function getHitPower(fighter) {
   // return hit power
+  const criticalHitChance = Math.random() + 1;
+  return fighter.attack * criticalHitChance;
 }
 
 export function getBlockPower(fighter) {
   // return block power
+  const dodgeChance = Math.random() + 1;
+  return fighter.defense * dodgeChance;
+}
+
+function getSuperShotPower(fighter) {
+  return fighter.attack * 2;
 }
 
 function createFighterStats({_id, health, defense, attack}) {
