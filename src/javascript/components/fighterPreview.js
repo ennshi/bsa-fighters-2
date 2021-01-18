3ee0e37c7469f4ef750eecfaa3445ce8995f9c64
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-18 11:31:53 +0100

    Display fighter details

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -8,7 +8,21 @@ export function createFighterPreview(fighter, position) {
   });
 
   // todo: show fighter info (image, name, health, etc.)
-
+  if(fighter) {
+    const fighterImg = createFighterImage(fighter);
+    const fighterDetailsWrapper = createElement({
+      tagName: 'div',
+      className: 'fighter-details__wrapper'
+    });
+    const fighterName = createElement({
+      tagName: 'h2',
+      className: 'fighter-details__title'
+    });
+    fighterName.innerText = fighter.name;
+    const fighterStats = createFighterStats(fighter);
+    fighterDetailsWrapper.append(fighterName, fighterStats);
+    fighterElement.append(fighterImg, fighterDetailsWrapper);
+  }
   return fighterElement;
 }
 
@@ -26,4 +40,41 @@ export function createFighterImage(fighter) {
   });
 
   return imgElement;
+}
+
+function createFighterStats(fighter) {
+  const fighterStats = createElement({
+    tagName: 'div',
+    className: 'fighter-details__stats'
+  });
+  const healthCell = createFighterStatCell('health', fighter.health);
+  const attackCell = createFighterStatCell('attack', fighter.attack);
+  const defenseCell = createFighterStatCell('defense', fighter.defense);
+  fighterStats.append(healthCell, attackCell, defenseCell);
+  return fighterStats;
+}
+
+function createFighterStatCell(label, value) {
+  const cell = createElement({
+    tagName: 'div',
+    className: 'fighter-details__stat-cell'
+  });
+  const icon = createStatIcon(label);
+  const valueElement = createElement({
+    tagName: 'p'
+  });
+  valueElement.innerText = value;
+  cell.append(icon, valueElement);
+  return cell;
+}
+
+function createStatIcon(label) {
+  return createElement({
+    tagName: 'img',
+    className: 'fighter-details__icon',
+    attributes: {
+      src: `../../../resources/icons/${label}.png`,
+      alt: label
+    }
+  });
 }
